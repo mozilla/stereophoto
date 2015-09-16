a86f7be1ac1e20b59ba351ff67f78a45f0cb5823
Piotr Zalewa <piotr@zalewa.info>
2015-09-16 13:08:40 +0200

    displaying video (working, but not all the time)

--- ember-stereophoto/app/components/stereos/show-stream.js
+++ ember-stereophoto/app/components/stereos/show-stream.js
@@ -2,11 +2,40 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement: function() {
-    console.log('DEBUG: didInsertElement in component', this.$('hey'));
+    // show video stream in <video> element
+    var v = document.getElementById('video-stream');
+    navigator.getMedia = (navigator.getUserMedia ||
+                          navigator.webkitGetUserMedia ||
+                          navigator.mozGetUserMedia ||
+                          navigator.msGetUserMedia);
+    navigator.getMedia( 
+      {video: true, audio: false},
+      function(stream) {
+        if (navigator.mozGetUserMedia) {
+          v.mozSrcObject = stream;
+        } else {
+          var vendorURL = window.URL || window.webkitURL;
+          v.src = vendorURL.createObjectURL(stream);
+        }
+        v.play();
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );
+
+    // video.addEventListener('canplay', function(ev){
+      // there might be a way to get resolution from this
+    // }, false);
   },
   actions: {
     takePhoto: function() {
-      console.log('action from component');
+      console.log('DEBUG: taking the photo in component');
+      var video = document.getElementById('video-stream');
+      var canvas = document.getElementById('offscreen-canvas');
+      var width = video.width;
+      console.log('DEBUG: videoElementWidth', width, 'videoWidth', video.videoWidth);
+      var data = canvas.toDataURL('image/png');
       // disable stream
       this.sendAction();
     }
